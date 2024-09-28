0e6a6ce07f0c282a41fdf17dc8d0fbb3d0dd7fe2
PatrycjaKoszyk99 <kospat1999@gmail.com>
2024-09-28 20:55:35 +0200

    mocks

--- src/views/RouteDetailsView.vue
+++ src/views/RouteDetailsView.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts">
 import { useRoutesStore } from '../stores/routes'
+import { Carousel, Navigation, Pagination, Slide } from 'vue3-carousel'
+import 'vue3-carousel/dist/carousel.css'
 
 const store = useRoutesStore()
 </script>
 
 <template>
-  <main class="route-details-view">{{ store.selectedRoute }}</main>
+  <main class="route-details-view">
+    <Carousel>
+      <Slide v-for="slide in store.selectedRoute.images" :key="slide">
+        <div class="carousel__item"></div>
+      </Slide>
+
+      <template #addons>
+        <Navigation />
+        <Pagination />
+      </template>
+    </Carousel>
+  </main>
 </template>
 
 <style scoped lang="scss">
