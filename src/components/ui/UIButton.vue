32e66442e0cb23543379974e874cffdb97fa485f
PatrycjaKoszyk99 <kospat1999@gmail.com>
2024-09-28 22:27:06 +0200

    button

---
+++ src/components/ui/UIButton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+import { computed } from 'vue'
+
+const props = defineProps(['href', 'to'])
+
+const component = computed(() => {
+  if (props.href) {
+    return 'a'
+  }
+
+  if (props.to) {
+    return 'RouterLink'
+  }
+
+  return 'button'
+})
+</script>
+
+<template>
+  <component :is="component" />
+</template>
+
+<style scoped lang="scss"></style>
--- src/views/RouteDetailsView.vue
+++ src/views/RouteDetailsView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import UIButton from '@/components/ui/UIButton.vue'
 import { useRoutesStore } from '../stores/routes'
 import { Carousel, Navigation, Pagination, Slide } from 'vue3-carousel'
 import 'vue3-carousel/dist/carousel.css'
@@ -11,19 +12,51 @@ const store = useRoutesStore()
     <Carousel>
       <Slide v-for="slide in store.selectedRoute.images" :key="slide">
         <div class="carousel__item">
-          <img :src="`https://bikeyeah.jurczak.dev/img/${slide}.jpg`" />
+          <img :src="`https://yeahbike.jurczak.dev/img/${slide}.jpg`" class="slide-image" />
         </div>
       </Slide>
 
       <template #addons>
-        <Navigation />
         <Pagination />
       </template>
     </Carousel>
+    <UIButton />
   </main>
 </template>
 
 <style scoped lang="scss">
 .route-details-view {
+  padding: 0;
+}
+
+.carousel__pagination {
+  position: absolute;
+  bottom: 12px;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+:deep(.carousel__pagination-button::after) {
+  background-color: #ececeb;
+  opacity: 40%;
+  width: 8px;
+  height: 8px;
+  border-radius: 50%;
+  transition:
+    width 0.2s ease-in-out,
+    border-radius 0.2s ease-in-out;
+}
+
+:deep(.carousel__pagination-button--active::after) {
+  background-color: #ececeb;
+  opacity: 100%;
+  width: 28px;
+  height: 9px;
+  border-radius: 8px;
+}
+
+.slide-image {
+  min-height: 430px;
+  object-fit: cover;
 }
 </style>
