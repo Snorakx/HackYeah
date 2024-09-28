69e8ad780617e687aa401688a46465c79eb985b6
PatrycjaKoszyk99 <kospat1999@gmail.com>
2024-09-29 00:24:50 +0200

    wip

---
+++ src/components/details/MapButtons.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import UIButton from '@/components/ui/UIButton.vue'
+</script>
+
+<template>
+  <div class="map-buttons">
+    <div class="wrapper">
+      <UIButton type="green" class="button"> Pobierz </UIButton>
+
+      <UIButton type="green" class="button"> Mapa </UIButton>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.map-buttons .wrapper {
+  display: flex;
+  gap: 16px;
+  width: 100%;
+}
+
+.button {
+  flex: 1;
+}
+</style>
--- src/components/details/RouteDetails.vue
+++ src/components/details/RouteDetails.vue
@@ -6,28 +6,52 @@ const props = defineProps(['selectedRoute'])
   <div class="route-details">
     <div class="row">
       <div class="detail">
-        <h2>4,4 mili</h2>
+        <h2>{{ props.selectedRoute.distance }}</h2>
         <p>Odległość</p>
       </div>
 
       <div class="detail">
-        <h2>4,4 mili</h2>
-        <p>Odległość</p>
+        <h2>{{ props.selectedRoute.elevationGain }}</h2>
+        <p>Przewyższenie</p>
       </div>
     </div>
 
     <div class="row">
       <div class="detail">
-        <h2>4,4 mili</h2>
-        <p>Odległość</p>
+        <h2>{{ props.selectedRoute.duration }}</h2>
+        <p>Szacowany czas</p>
       </div>
 
       <div class="detail">
-        <h2>4,4 mili</h2>
-        <p>Odległość</p>
+        <h2>test</h2>
+        <p>Szlak pętlowy</p>
       </div>
     </div>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.route-details {
+  display: flex;
+  flex-direction: column;
+  padding: 24px;
+  gap: 16px;
+}
+
+.row {
+  display: flex;
+  gap: 16px;
+}
+
+.detail {
+  flex: 1;
+}
+
+h2 {
+  font: 500 32px/42px 'Roboto';
+}
+
+p {
+  font: 400 14px/16px 'Roboto';
+}
+</style>
