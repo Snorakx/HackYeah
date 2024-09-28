a251775283e5155b1ecfba64bbe2d9f2b34c148f
PatrycjaKoszyk99 <kospat1999@gmail.com>
2024-09-28 18:11:53 +0200

    start page

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,8 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import LogoSVG from './components/svg/LogoSVG.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <LogoSVG alt="Vue logo" />
-
-    <div>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
 </template>
 
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,5 +1,63 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
 
 <template>
-  <main>test123</main>
+  <main class="login-page">
+    <h1 class="title">Witaj trasa!</h1>
+    <p class="description">Jedź bezpiecznie rowerem blablabla</p>
+    <RouterLink to="/about" class="login-button">Zacznij teraz</RouterLink>
+  </main>
 </template>
+
+<style scoped lang="scss">
+.login-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 12px;
+  min-height: 100vh;
+
+  .title {
+    font: 500 36px/44px 'Roboto';
+  }
+
+  .description {
+    font: 400 16px/24px 'Roboto';
+    color: gray;
+  }
+
+  .login-button {
+    border: 1px solid #dadada;
+    background-color: #f6f6f6;
+    font: 400 16px/24px 'Roboto';
+    border-radius: 40px;
+    padding: 8px;
+    width: 100%;
+    min-width: 280px;
+    align-items: center;
+    justify-content: center;
+    display: flex;
+    color: #323232;
+    position: relative;
+    min-height: 64px;
+    margin-top: 120px;
+
+    &::before {
+      content: url(../images/arrow-right.svg);
+      background-color: #222222;
+      padding: 12px;
+      display: flex;
+      left: 8px;
+      position: absolute;
+      border-radius: 50%;
+      width: 48px;
+      height: 48px;
+    }
+
+    &:hover {
+    }
+  }
+}
+</style>
